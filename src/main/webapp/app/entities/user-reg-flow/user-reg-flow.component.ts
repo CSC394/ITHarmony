7062a0a646627bae06cef4ce56cdd63685404635
Jonathan Sandau <jjs1mail@gmail.com>
2018-03-11 15:35:39 -0500

    Added a form to User Reg flow, first step is functional

--- src/main/webapp/app/entities/user-reg-flow/user-reg-flow.component.ts
+++ src/main/webapp/app/entities/user-reg-flow/user-reg-flow.component.ts
@@ -5,18 +5,29 @@ import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 import { Principal } from '../../shared';
 
+import { UserProfileExtraItharmony } from '../../entities/user-profile-extra-itharmony/user-profile-extra-itharmony.model';
+import { UserTypeT } from 'src/main/webapp/app/entities/user-profile-extra-itharmony';
+import { UserProfileExtraItharmonyService } from '../user-profile-extra-itharmony/user-profile-extra-itharmony.service';
+import { Router } from '@angular/router';
+
 @Component({
     selector: 'jhi-user-reg-flow',
     templateUrl: './user-reg-flow.component.html'
 })
 export class UserRegFlowComponent implements OnInit, OnDestroy {
     currentAccount: any;
-    eventSubscriber: Subscription;
+
+    userProfileExtra: UserProfileExtraItharmony;
+    currentRole: any;
+
+    roles = ['CANDIDATE', 'COMPANY'];
 
     constructor(
         private jhiAlertService: JhiAlertService,
         private eventManager: JhiEventManager,
-        private principal: Principal
+        private principal: Principal,
+        private userProfileExtraService: UserProfileExtraItharmonyService,
+        private router: Router
     ) {
     }
 
@@ -28,11 +39,28 @@ export class UserRegFlowComponent implements OnInit, OnDestroy {
     }
 
     ngOnDestroy() {
-        this.eventManager.destroy(this.eventSubscriber);
-    }
 
+    }
 
     private onError(error) {
         this.jhiAlertService.error(error.message, null, null);
+    }
+
+    public setUserRole(user) {
+        this.currentRole = user;
+    }
+
+    public dothestupidthing = () => {
+        this.userProfileExtra = new UserProfileExtraItharmony();
+        if (this.currentRole === 'Company') {
+            this.userProfileExtra.userTypeT = this.currentRole;
+            console.warn('a');
+        } else {
+            this.userProfileExtra.userTypeT = this.currentRole;
+            console.warn('b');
+        }
+
+        console.warn(this.userProfileExtra);
+        console.warn(this.userProfileExtraService.create(this.userProfileExtra).subscribe((val) => this.router.navigate(['/user-profile-extra-itharmony'])));
     }
 }
