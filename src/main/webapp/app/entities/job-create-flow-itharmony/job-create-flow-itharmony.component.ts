6fd7a7ba4788d104f71484fd39df7e81c14fa2cd
Matt Hallowell <matthewhallowell1@gmail.com>
2018-03-16 02:22:08 -0500

    jobs can be created and put within the database.  needs further work

--- src/main/webapp/app/entities/job-create-flow-itharmony/job-create-flow-itharmony.component.ts
+++ src/main/webapp/app/entities/job-create-flow-itharmony/job-create-flow-itharmony.component.ts
@@ -10,8 +10,9 @@ import { JobPostItharmonyService } from '../job-post-itharmony/job-post-itharmon
 import { SkillsProfileItharmony } from '../skills-profile-itharmony';
 import { SkillsProfileItharmonyService } from '../skills-profile-itharmony';
 import { UserProfileExtraItharmonyService } from '../user-profile-extra-itharmony/user-profile-extra-itharmony.service';
-import { UserProfileExtraItharmony } from '../user-profile-extra-itharmony/user-profile-extra-itharmony.model'
+import { UserProfileExtraItharmony } from '../user-profile-extra-itharmony/user-profile-extra-itharmony.model';
 import { Router } from '@angular/router';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
     selector: 'jhi-job-create-flow-itharmony',
@@ -45,8 +46,8 @@ export class JobCreateFlowItharmonyComponent implements OnInit, OnDestroy {
                 let alreadyfound = false;
                 for (const upe of res.body){
                     if (upe.userId === this.currentAccount.id) {
-                        console.log('UserProfileExtra Identified with the current account');
-                        console.log(upe.userId + ' ' + this.currentAccount.id + ' ' + upe.userTypeT);
+                        console.log('UserProfileExtra Identified with the current account.');
+                        console.log('UserProfileExtra.userID: ' + upe.userId + ' currentAccount.id: ' + this.currentAccount.id + ' AccountType: ' + upe.userTypeT);
                         this.userProfileExtra = upe;
                         alreadyfound = true;
                         break;
@@ -55,11 +56,34 @@ export class JobCreateFlowItharmonyComponent implements OnInit, OnDestroy {
                 if (!alreadyfound) {
                     console.warn('UserProfileExtra does not exist with this ID!)' + this.currentAccount.id);
                 }
-            }, (res) => { console.warn('Error Querying the UserProfileExtraService');
+            }, (res) => {
+                console.warn('Error Querying the UserProfileExtraService');
             });
         });
     }
 
     ngOnDestroy() {
+    }
+
+    save() {
+        console.log('Save called on Job Creation Form.');
+        this.isSaving = true;
+        if (this.jobPost.id !== undefined) {
+                console.log('Saving successfully with: ' + this.jobPost);
+                this.subscribeToSaveResponse(this.jobPostItharmonyService.update(this.jobPost));
+            } else {
+                this.subscribeToSaveResponse(this.jobPostItharmonyService.create(this.jobPost));
+            }
+    }
+
+    private subscribeToSaveResponse(result: Observable<HttpResponse<JobPostItharmony>>) {
+        result.subscribe((res: HttpResponse<JobPostItharmony>) =>
+            this.onSaveSuccess(res.body));
+    }
+
+    private onSaveSuccess(result: JobPostItharmony) {
+        this.eventManager.broadcast({name: 'JobPostItHarmony', content: 'OK'});
+        this.isSaving = false;
+        console.log('Save was successful!');
     }
 }
